a8a6d5a463e8723803ccf357e95e1da2c40943ba
Diana Hakim <w0420915@nscc.ca>
2022-09-19 19:12:29 -0300

    Module 01, Section 02: Passcode

---
+++ Lab01/Module01/Section02/Login/Login/Program.cs
@@ -0,0 +1,56 @@
+﻿// Author Diana
+// NSCC
+// Sept 19, 2022
+// Test if the code entered is correct
+
+using System;
+
+namespace Login
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //----------------------Simplifed Video-------------------------------
+            // Create the code variable 
+            string code = "secret";
+
+            // Get input
+            Console.WriteLine("What is the pass code?");
+            var codeInput = Console.ReadLine();
+
+            // Test if correct
+            bool correct = codeInput == code; //test if the code was correct
+            Console.WriteLine(correct ? "Authenticted" : "Not Authenticted");// no need to write brackets for one command  
+
+            //----------------------------Video-----------------------------------
+            //Console.WriteLine("What is the pass code?");
+            //var codeInput = Console.ReadLine();
+            //if (codeInput == "secert")
+            //{
+            //    Console.WriteLine("Authenticted");
+            //}
+            //else if (codeInput != "secret") 
+            //{
+            //    Console.WriteLine("Not Authenticted");
+            //}
+
+
+            //----------------Testing how I'd fix it---------------------------------------------
+            //SecureString theSecureString = new NetworkCredential("", "secret").SecurePassword;
+            //bool correct = false;
+
+            //while (!loop)
+            //{
+            //    Console.WriteLine("What is the pass code?");
+            //    var codeInput = Console.ReadLine();
+            //    correct = (codeInput == new NetworkCredential("", theSecureString).Password) ? true : false;
+            //    Console.WriteLine(correct ? "Authenticted" : "Not Authenticted");
+            //}
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Lab01/Module01/Section02/Zodiac/Zodiac/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Zodiac
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("What is your age?");
+            var age = Console.ReadLine();
+
+            Console.WriteLine("What month were you born?");
+            var month = Console.ReadLine();
+
+            Console.WriteLine("What is your email?");
+            var email = Console.ReadLine();
+
+            Console.WriteLine("What date were you born?");
+            var date = Console.ReadLine();
+
+
+
+            Console.WriteLine("Your name is: {0}", name);
+            Console.WriteLine("Your name is: {0}", age);
+            Console.WriteLine("Your name is: {0}", month);
+            Console.WriteLine("Your name is: {0}", email);
+            Console.WriteLine("Your name is: {0}", date);
+
+            Console.ReadLine(); // Keep open
+
+            if (month == "march") 
+            {
+                Console.WriteLine("You are an Aries");
+            }
+            else if (month == "april") 
+            {
+                Console.WriteLine("You are a Taurus");
+            }
+            else if (month == "may")
+            {
+                Console.WriteLine("You are a Gemini");
+            }
+
+        }
+    }
+}
