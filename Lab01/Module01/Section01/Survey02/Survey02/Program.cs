ec40d31148d9f97c3a68056d82bceea7f9b72c4b
Diana Hakim <w0420915@nscc.ca>
2022-09-19 16:06:19 -0300

    Module 01, Section 01: Survey Complete 2

---
+++ Lab01/Module01/Section01/Survey02/Survey02/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Survey02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("What is your age?");
+            var age = Console.ReadLine();
+
+            Console.WriteLine("What month were you born?");
+            var month = Console.ReadLine();
+
+            Console.WriteLine("What is your email?");
+            var email = Console.ReadLine();
+
+            Console.WriteLine("What date were you born?");
+            var date = Console.ReadLine();
+
+
+
+            Console.WriteLine("Your name is: {0}", name);
+            Console.WriteLine("Your name is: {0}", age);
+            Console.WriteLine("Your name is: {0}", month);
+            Console.WriteLine("Your name is: {0}", email);
+            Console.WriteLine("Your name is: {0}", date);
+
+
+
+        }
+    }
+}
